29ee4d843f0e5b3a22037b6fb2e11ee90aa59ead
Joselu <joseluissgz97@gmail.com>
2024-02-05 19:06:07 +0100

    destination page

--- src/app/pages/destination-page/destination-page.component.ts
+++ src/app/pages/destination-page/destination-page.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { map } from 'rxjs';
 import { HeaderComponentComponent } from '../../components/header-component/header-component.component';
+import { Destination } from '../../interfaces/db-interface';
+import { SpaceTurismServiceService } from '../../services/space-turism-service.service';
 
 @Component({
   selector: 'app-destination-page',
   standalone: true,
-  imports: [HeaderComponentComponent],
+  imports: [CommonModule, HeaderComponentComponent],
   templateUrl: './destination-page.component.html',
   styleUrl: './destination-page.component.scss',
 })
-export class DestinationPageComponent {}
+export class DestinationPageComponent implements OnInit {
+  private spaceTurismServiceService = inject(SpaceTurismServiceService);
+
+  public destination?: Destination[];
+
+  ngOnInit(): void {
+    this.spaceTurismServiceService.data$
+      .pipe(map((data) => data.destinations))
+      .subscribe((destinations) => {
+        this.destination = destinations;
+        console.log('destinations', this.destination);
+      });
+  }
+}
