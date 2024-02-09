e9d4b3e619f172b1caca3cd647366cf3a9ee8b66
Joselu <joseluissgz97@gmail.com>
2024-02-09 11:09:39 +0100

    Crew page

--- src/app/pages/crew-page/crew-page.component.ts
+++ src/app/pages/crew-page/crew-page.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { map } from 'rxjs';
 import { HeaderComponentComponent } from '../../components/header-component/header-component.component';
+import { Crew } from '../../interfaces/db-interface';
+import { SpaceTurismServiceService } from '../../services/space-turism-service.service';
 
 @Component({
   selector: 'app-crew-page',
   standalone: true,
-  imports: [HeaderComponentComponent],
+  imports: [CommonModule, HeaderComponentComponent],
   templateUrl: './crew-page.component.html',
   styleUrl: './crew-page.component.scss',
 })
-export class CrewPageComponent {}
+export class CrewPageComponent implements OnInit {
+  private spaceTurismServiceService = inject(SpaceTurismServiceService);
+
+  public crew?: Crew[];
+  public transformValue = '0%';
+
+  ngOnInit() {
+    this.spaceTurismServiceService.data$
+      .pipe(map((data) => data.crew))
+      .subscribe((crew) => {
+        console.log('crew', crew);
+        this.crew = crew;
+      });
+  }
+
+  public nextPerson(index: number) {
+    console.log(index);
+    let position = index;
+    let opration = position * -25;
+
+    this.transformValue = `${opration}%`;
+  }
+}
