134ef616a37e49fa957979e1f336662f0100db7c
Joselu <joseluissgz97@gmail.com>
2024-02-12 16:59:17 +0100

    technology page

--- src/app/pages/technology-page/technology-page.component.ts
+++ src/app/pages/technology-page/technology-page.component.ts
@@ -1,11 +1,40 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { map } from 'rxjs';
 import { HeaderComponentComponent } from '../../components/header-component/header-component.component';
+import { Technology } from '../../interfaces/db-interface';
+import { SpaceTurismServiceService } from '../../services/space-turism-service.service';
 
 @Component({
   selector: 'app-technology-page',
   standalone: true,
-  imports: [HeaderComponentComponent],
+  imports: [CommonModule, HeaderComponentComponent],
   templateUrl: './technology-page.component.html',
   styleUrl: './technology-page.component.scss',
 })
-export class TechnologyPageComponent {}
+export class TechnologyPageComponent implements OnInit {
+  private spaceTurismService = inject(SpaceTurismServiceService);
+
+  public transformValue: string = '0%';
+  public operation = 100 / 3;
+
+  public technologys?: Technology[];
+  public isActiveNav: number = 0;
+
+  ngOnInit(): void {
+    this.spaceTurismService.data$
+      .pipe(map((value) => value.technology))
+      .subscribe((technology) => {
+        this.technologys = technology;
+      });
+  }
+
+  public nextTechnolgy(index: number) {
+    let position: number = index;
+    this.isActiveNav = index;
+
+    let caltculateTranslate = position * -this.operation;
+
+    this.transformValue = `${caltculateTranslate}%`;
+  }
+}
